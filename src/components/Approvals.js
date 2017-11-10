29930402722c2696a8375c0286b01e6ac49f3fc3
HeenaChampaneri <heena@triveniconsulting.com>
2017-11-10 13:37:16 -0500

    commit

---
+++ src/components/Approvals.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import approvalGrid from 'images/approvals.PNG'
+
+import {
+    Container,
+    TabContent,
+    TabPane,
+    Card,
+    Table,
+    Collapse,
+    CardBody,
+    Button,
+    CardTitle,
+    CardText,
+    Row,
+    Col,
+    CardImg
+    
+} from "reactstrap";
+
+export default class ApprovalsTab extends Component {
+
+    render() {
+        return(
+            <Container fluid style={{ width: 1024, overflow: "hidden" }} >
+            <div className="d-flex">
+                <Row> <Col sm="12">
+                <Card style={{ width: 900} }>
+                 <CardImg
+                
+                style={{width:"100%",height:"100%"}}
+                    src={approvalGrid}                
+              />
+            </Card>
+                </Col>
+
+                </Row>
+            </div>
+            </Container>
+        )
+    }
+
+}
