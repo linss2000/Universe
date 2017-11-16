5ba07fc68e45917d45ffcc7c1d7b6637dfd2e7fb
Venugopal Kolli <Venu@Venugopals-MacBook-Pro.local>
2017-11-16 11:12:51 -0500

    numbers

--- src/reducers/notificationsreducer.js
+++ src/reducers/notificationsreducer.js
@@ -2,21 +2,21 @@ export default function() {
     return [
         
         {
-        total_ntf_cnt : 5 ,
+        total_ntf_cnt : 4 ,
         notify_type : 'New Cadet Applications submitted.',
-        notify_message : 'There have been 5 cadet applications submitted since you last logged in.',
+        notify_message : 'There have been 4 cadet applications submitted since you last logged in.',
         nofity_details : 'View New Cadet Applications'
         },
         {
-         total_ntf_cnt : 3 ,
+         total_ntf_cnt : 2 ,
          notify_type : 'Courses Requiring Approval ',
-         notify_message : 'There  are 3 new courses requiring your review and approval.',
+         notify_message : 'There  are 2 new courses requiring your review and approval.',
          nofity_details : 'Approve/Reject Courses'
         },
          {
          total_ntf_cnt : 4 ,
          notify_type : 'Budget Items Requiring Approval',
-         notify_message : 'There are 2 new budget items requiring your review and approval.',
+         notify_message : 'There are 4 new budget items requiring your review and approval.',
          nofity_details : 'Approve/ Reject Budget Items'
         }
 
