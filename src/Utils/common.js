ccb9f6c3a06115b850033b3e84bde34b886cea83
HeenaChampaneri <heena@triveniconsulting.com>
2018-02-09 11:36:07 -0500

    Common methods to parse the functions and check if user has permission or not

--- src/Utils/common.js
+++ src/Utils/common.js
@@ -2,7 +2,7 @@ const _ = require("lodash");
 
 function isJSON(retVal) {
     //alert(sessionStorage.getItem("token"))
-    
+
     try {
         //console.log("5555555")
         //console.log(this.props)
@@ -15,14 +15,40 @@ function isJSON(retVal) {
                 sessionStorage.setItem("token", retObj.token);
             }
             if(retObj.roles){
-                sessionStorage.setItem("roles", retObj.roles);
+                sessionStorage.setItem("roles", JSON.stringify(retObj.roles));
+              //  sessionStorage.setItem("roles", retObj.roles);
             }
         }
       return (JSON.parse(retVal) && !!retVal);
-    } catch (e) {    
-        //alert(e)    
+    } catch (e) {
+        //alert(e)
       return false;
     }
-  } 
+  }
 
-module.exports = { isJSON };
+  /*  Can include in the reducer of each page
+    export const permissions = [
+     {function_id : 1, action : 'ADDEDIT'},
+     {function_id : 2 ,action: 'VIEW'} ,
+    {function_id : 3 , action : "DELETE"}  
+  ]
+  */
+  function getPermissions(permissions) {
+    let functions = JSON.parse(sessionStorage.getItem("roles"));
+    debugger
+   let flist = _.uniqBy(_.filter(functions, function(o)
+   {return (_.find(permissions, {'function_id' : o.function_id} )) }
+  ),'function_id');
+  // flist =  _.filter(flist,{'function_id':2});
+    return flist;
+  }
+
+  function checkPermission(permissions, function_id) {
+    let pList = getPermissions(permissions);
+     if( _.findIndex( pList , {'function_id' : function_id}) > -1 )
+       return true;
+    else
+      return false;
+  }
+
+module.exports = { isJSON , getPermissions , checkPermission };
